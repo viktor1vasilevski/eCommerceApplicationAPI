48117c5cadeaa3ef325f275a37ca0312d4a7638c
vik_vas <vasilevskiviktor@yahoo.com>
2025-01-25 23:52:42 +0100

    init sql database

---
+++ Data/Context/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Data.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> dbContext) : base(dbContext)
+    {
+            
+    }
+}
